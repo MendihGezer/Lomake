8d3feb39e4004268211e3798c2ad09694cff4134
unknown <mendih.gezer@edu.keuda.fi>
2020-09-16 09:47:49 +0300

    java

--- proj1b.js
+++ proj1b.js
@@ -9,7 +9,8 @@ var email = form.email.value;
 var pnumero = form.pnumero.value;
 var gender = form.gender;
 var date = form.date.value;
-var select = form.select.value;
+var worktime = form.worktime;
+
 
 if(etunimi.length<2)
 {
@@ -67,8 +68,21 @@ if (date == "")
   form.date.focus();
   return false;
 }
-}
+var vastaus=false;
 
+for (var i=0; i < worktime.length;i++)
+{
+if (worktime[i].checked==true)
+{
+vastaus=true;
+}
+}
+if(vastaus==false)
+{
+alert("Valitse työn tyyppi!");
+return false;
+}
+}
 
 
 
